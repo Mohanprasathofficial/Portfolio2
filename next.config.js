1330ca3a668ae5b885d1830d004602995602ddda
Mohanprasath S R <you@example.com>
2026-04-23 21:23:11 +0530

    fixed build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+
   images: {
-    domains: ['example.com'],
+    domains: ['example.com'], // keep only if you use external images
   },
 };
 
